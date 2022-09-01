0bc1ec481eaf81ecfabfe4d929ab3799a1242c26
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-08-31 22:15:54 -0700

    incoming video data being written to JSON file.  JSON file being read to return data.

--- routes/videos.js
+++ routes/videos.js
@@ -1,31 +1,70 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { off } = require("process");
 const { v4: uuid4 } = require("uuid");
 
-const videos = require("../data/videos.json");
+/**
+ * Load the video list from the JSON file
+ * @param {function} callback
+ */
+const loadVideos = (callback) => {
+  fs.readFile("./data/videos.json", "utf8", callback);
+};
+
+const writeVideos = (callback) => {
+  fs.writeFile("./data/videos.json", callback, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.info(
+        loadVideos((err, data) => {
+          if (err) {
+            console.error(err);
+          }
+        })
+      );
+    }
+  });
+};
 
 /**
  * GET endpoint to return a simplified list of videos
  */
 router.get("/", (req, res) => {
-  res.json(
-    videos.map((vid) => {
-      return {
-        id: vid.id,
-        title: vid.title,
-        channel: vid.channel,
-        image: vid.image,
-      };
-    })
-  );
+  loadVideos((err, data) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    } else {
+      const videoList = JSON.parse(data);
+      res.json(
+        videoList.map((vid) => {
+          return {
+            id: vid.id,
+            title: vid.title,
+            channel: vid.channel,
+            image: vid.image,
+          };
+        })
+      );
+    }
+  });
 });
 
 /**
  * GET endpoint to return the full details for a specific video.
  */
 router.get("/:id", (req, res) => {
-  res.json(videos.find((vid) => vid.id === req.params.id));
+  loadVideos((err, data) => {
+    if (err) {
+      console.error(err);
+      res.send("error finding the video!");
+    } else {
+      const videoList = JSON.parse(data);
+      res.json(videoList.find((vid) => vid.id === req.params.id));
+    }
+  });
 });
 
 /**
@@ -46,8 +85,16 @@ router.post("/", (req, res) => {
     timestamp: Date.now(),
     comments: [],
   };
-  videos.push(newVideo);
-  res.json(videos);
+  loadVideos((err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const videoList = JSON.parse(data);
+      videoList.push(newVideo);
+      writeVideos(JSON.stringify(videoList));
+      res.status(201).json(newVideo);
+    }
+  });
 });
 
 module.exports = router;
